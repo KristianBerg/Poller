782c0302ee85a462bcc33d9d8dcc6e4de4c83060
KristianBerg <kristianberg.jobb@gmail.com>
2015-10-14 20:26:45 +0200

    adds +1 to an answer, also buttons

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("Hello world!")
+
+var url = "https://poller-kristian.herokuapp.com/poll"
+$.getJSON(url, {question: 'test2', value: 'tjena tjohopp'}, function(json, textStatus) {
+  console.log('Result', json);
+});
+
+var url = "https://poller-kristian.herokuapp.com/result"
+$.getJSON(url, {question: 'test2'}, function(json, textStatus) {
+  console.log('Result', json);
+});
