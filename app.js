594674a5d933e2a3be583679b6239ad8d560b0a9
KristianBerg <kristianberg.jobb@gmail.com>
2015-10-14 20:51:50 +0200

    App now registers responses to questions

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ console.log("Hello world!")
 
 function getResult(){
   var url = "https://poller-kristian.herokuapp.com/result"
-  $.getJSON(url, {question: 'test2'}, function(json, textStatus) {
+  $.getJSON(url, {question: 'whatisyourquest'}, function(json, textStatus) {
     console.log('Result', json);
   });
 }
 
 function submitAnswer(element){
   var url = "https://poller-kristian.herokuapp.com/poll"
-  $.getJSON(url, {question: 'test2', value: 'tjena tjohopp'}, function(json, textStatus) {
+  var $el = $(element)
+  var val = $el.attr('data-value')
+  $.getJSON(url, {question: 'whatisyourquest', value: val}, function(json, textStatus) {
     console.log('Result', json);
   });
 }
