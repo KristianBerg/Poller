1473d93224ed8082ff3a7b6c3638a54b8afae79a
KristianBerg <kristianberg.jobb@gmail.com>
2015-10-14 20:37:15 +0200

    created functions getResult and submitAnswer

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 console.log("Hello world!")
 
-var url = "https://poller-kristian.herokuapp.com/poll"
-$.getJSON(url, {question: 'test2', value: 'tjena tjohopp'}, function(json, textStatus) {
-  console.log('Result', json);
-});
 
-var url = "https://poller-kristian.herokuapp.com/result"
-$.getJSON(url, {question: 'test2'}, function(json, textStatus) {
-  console.log('Result', json);
-});
+
+function getResult(){
+  var url = "https://poller-kristian.herokuapp.com/result"
+  $.getJSON(url, {question: 'test2'}, function(json, textStatus) {
+    console.log('Result', json);
+  });
+}
+
+function submitAnswer(element){
+  var url = "https://poller-kristian.herokuapp.com/poll"
+  $.getJSON(url, {question: 'test2', value: 'tjena tjohopp'}, function(json, textStatus) {
+    console.log('Result', json);
+  });
+}
